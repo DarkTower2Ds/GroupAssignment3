a9db90bc0894577338988ceebc655f703be32c28
PichvydaTuy <31899661+PichvydaTuy@users.noreply.github.com>
2017-10-07 14:16:16 -0500

    Major Changes
    +Change the name of functions and elements in .h file to reflect those in the updated instruction
    
    +Finished Constructors, bookSeat(), checkSeat(), and clearSeat()
    
    +Changed the comments
    
    +Move the comments from above to below each function's prototype

--- Project1/Plane.cpp
+++ Project1/Plane.cpp
@@ -4,60 +4,91 @@ using namespace std;
 
 /*
 	Default Constructor
-	"Prompts the user to input the number of rows and columns of Seats
-	 for first class and economy class,
-	 then make a call to the overload constructor with those values."
+ • Ask for num of rows and cols of both firstClass and economy(using cin),
+   and store them in the relevant member variables.
+ • Initialize the double dimensional arrays for firstClass and economy
 */
 Plane::Plane()
 {
     int newFirstClassRow, newFirstClassCol;
     int newEconRow, newEconCol;
-    cout<<"How many rows for First Class?"<<endl;
+    
+    cout<<"How many rows for First Class?";
     cin>>newFirstClassRow;
     cout<<endl;
-    cout<<"How many columns for First Class?"<<endl;
+    cout<<"How many columns for First Class?";
     cin>>newFirstClassCol;
     cout<<endl;
-    
-    cout<<"How many rows for Economy?"<<endl;
+    cout<<"How many rows for Economy?";
     cin>>newEconRow;
     cout<<endl;
-    cout<<"How many columns for Economy?"<<endl;
+    cout<<"How many columns for Economy?";
     cin>>newEconCol;
     cout<<endl;
     
-    Plane(newFirstClassRow, newFirstClassCol,newEconRow, newEconCol);
+    
+    firstClassRows = newFirstClassRow;
+    firstClassCols = newFirstClassCol;
+    economyRows = newEconRow;
+    economyCols = newEconCol;
+    
+    firstClass[firstClassRows][firstClassCols];
+    economy[economyRows][economyCols];
+ 
 }
 
 /*
 	Overload Constructor
-	"Store all of the row and column data into the private variables.
-	 Initialize the 2D arrays firstClass and Economy."
+ • Store all the row and col data into the member variables.
+ • Initialize the double dimensional arrays for firstClass and economy
 */
 Plane::Plane(int newFirstClassRow,int newFirstClassCol, int newEconRow,int newEconCol){
-    firstClassRow = newFirstClassRow;
-    firstClassCol = newFirstClassCol;
-    econRow = newEconRow;
-    econCol = newEconCol;
-    Seat firstClass[firstClassRow][firstClassCol];
-    Seat economy[econRow][econCol];
+    firstClassRows = newFirstClassRow;
+    firstClassCols = newFirstClassCol;
+    economyRows = newEconRow;
+    economyCols = newEconCol;
+    firstClass[firstClassRows][firstClassCols];
+    economy[economyRows][economyCols];
+}
+
+int Plane::getFirstClassRows()
+{ return firstClassRows;}
+int Plane::getFirstClassCols()
+{ return firstClassCols;}
+                                            //Getters
+int Plane::getEconomyRows()
+{ return economyRows;}
+int Plane::getEconomyCols()
+{ return economyCols;}
+
+void setFirstClass(int Rows,int Cols){
+    firstClassRows = Rows;
+    firstclassCols = Cols;
+}                                           //Setters
+void setEconomyClass(int Rows,int Cols){
+    economyRows = Rows;
+    economyCols = Cols;
 }
 
 
-/*
-	"Show all the seats in correct order (both first class and economy class)"
-*/
-void Plane::showAllSeat()
+void Plane::displaySeats()
 {
-
+    /*
+     
+     • Print (formatted neatly) all the seats in correct order (both firstClass and economy)
+     • The user should be able to tell from the display which seats are available
+     and which are booked
+     
+     */
 }
 
-/*
-	"Ask if the user wants first class or economy class.
-	 Ask for the Row and Column they want.
-	 If the seat was not booked, book it. Otherwise, inform the user is booked."
-*/
-void Plane::bookASeat(){
+
+void Plane::bookSeat(){
+    /*
+     • ask whether a user wants a seat in firstClass or economy
+     • ask for the row they want to sit in, and the seat number in that row
+     • If seat is available, book it. Else, inform user that it’s already booked
+     */
     int choice, row, column;
     cout<<"First Class(1) or Economy(2)?";
     cin>>choice;
@@ -70,12 +101,12 @@ void Plane::bookASeat(){
         cin>>column;
         cout<<endl;
         
-        if((firstClass[row][column].isEmpty == true) || (firstClass[row][column].booking = '-')){
+        if((firstClass[row][column].isBooked == false) || (firstClass[row][column].status = '-')){
             cout<<"Thank you, your booked seat is at Row: "<<row<<" Column: "<<column<<endl;
-            firstClass[row][column].booking = 'x';
+            firstClass[row][column].status = 'X';
         }
         else{
-            cout<<"Sorry, this seat is already booked.";
+            cout<<"Sorry, this seat is already booked."<<endl;
         }
     }
     
@@ -87,42 +118,127 @@ void Plane::bookASeat(){
         cin>>column;
         cout<<endl;
         
-        if((economy[row][column].isEmpty == true) || (economy[row][column].booking = '-')){
+        if((economy[row][column].isBooked == false) || (economy[row][column].status = '-')){
             cout<<"Thank you, your booked seat is at Row: "<<row<<" Column: "<<column<<endl;
-            economy[row][column].booking = 'x';
+            economy[row][column].booking = 'X';
         }
         else{
             cout<<"Sorry, this seat is already booked.";
-        }
+    }
+    else{
+        cout<<"Sorry, your input is invalid. Please choose again."<<endl;
+        bookSeat();
     }
 }
 
 
-/*
-	"Ask if the user wants [to check] first class or economy class.
-	 Ask for the row and column [of the Seat they want to check].
-	 Inform the user [whether] the seat is booked."
-*/
-void Plane::checkASeat()
+
+void Plane::checkSeat()
 {
+    /*
+     • ask if user want firstClass or economy seat
+     • ask for the row/col numbers of the seat they want to check
+     • Inform user whether or not the seat is booked
+     */
+    int choice, row, column;
+    cout<<"First Class(1) or Economy(2)?";
+    cin>>choice;
+    
+    if(choice == 1){
+        cout<<"You're now checking for a First Class seat"<<endl;
+        cout<<"Which Row?: ";
+        cin>>row;
+        cout<<"Which Column?: ";
+        cin>>column;
+        cout<<endl;
+        
+        if((firstClass[row][column].isBooked == false) || (firstClass[row][column].status = '-')){
+            cout<<"The seat is available for booking!";
+        }
+        else{
+            cout<<"Sorry, this seat is already booked."<<endl;
+        }
+    }
+    
+    else if(choice == 2){
+        cout<<"You're now checking for an Economy seat"<<endl;
+        cout<<"Which Row?: ";
+        cin>>row;
+        cout<<"Which Column?: ";
+        cin>>column;
+        cout<<endl;
+        
+        if((economy[row][column].isBooked == false) || (economy[row][column].status = '-')){
+            cout<<"The seat is available for booking!";
+        }
+        else{
+            cout<<"Sorry, this seat is already booked."<<endl;
+    }
+    else{
+        cout<<"Sorry, your input is invalid. Please choose again."<<endl;
+        checkSeat();
+    }
+    
+}
+
+
+void Plane::clearSeat()
+{
+    /*
+     • ask whether user wants to clear firstClass or economy seat
+     • ask for the row/col numbers of the seat they want to clear
+     • If booked, empty the seat. Else, inform user that seat was not booked
+     */
+    int choice, row, column;
+    cout<<"Would you like to clear: First Class seat (1) or Economy sea (2)?";
+    cin>>choice;
+    
+    if(choice == 1){
+        cout<<"You're now clearing a First Class seat"<<endl;
+        cout<<"Which Row?: ";
+        cin>>row;
+        cout<<"Which Column?: ";
+        cin>>column;
+        cout<<endl;
+        
+        if((firstClass[row][column].isBooked == true) || (firstClass[row][column].status = 'X')){
+            firstClass[row][column].status = '-';
+        }
+        else{
+            cout<<"This is an empty seat."<<endl;
+        }
+    }
+    
+    else if(choice == 2){
+        cout<<"You're clearing an Economy seat"<<endl;
+        cout<<"Which Row?: ";
+        cin>>row;
+        cout<<"Which Column?: ";
+        cin>>column;
+        cout<<endl;
+        
+        if((economy[row][column].isBooked == true) || (economy[row][column].status = 'X')){
+            economy[row][column].status = '-';
+        }
+        else{
+            cout<<"This is an empty seat."<<endl;
+        }
+    }
+    else{
+        cout<<"Sorry, your input is invalid. Please choose again."<<endl;
+        clearSeat();
+        }
+        
+    }
 
 }
 
-/*
-	"Ask if the user if they want [to clear] first class or economy class.
-	 Ask for the row and column [of the Seat they want to clear].
-	 If the Seat is booked, empty the Seat. Else, inform the user the Seat is empty.
-*/
-void Plane::clearASeat()
-{
 
-}
-
-/*
-	"Clean all of the Seats on the Plane and put [them] back to empty."
-*/
-void Plane::emptyPlane()
+void Plane::clearAllSeats()
 {
+    /*
+     • clean all seats on the plane and put it back to empty
+     */
 	Seat **rowIterator = firstClass;
 	for (int i = 0; i < firstClassRow; i++)
 	{
@@ -154,12 +270,14 @@ void Plane::emptyPlane()
 	}
 }
 
-/*
-	"Delete all Seats in the Plane.
-	 [This] will be called when the user wants to exit the program."
-*/
+
 Plane::~Plane()
 {
+  /*
+  • Delete all seats in the plane
+  • MUST Delete two dimensional arrays
+  • Will be called when the user exits the program
+  */
 	delete [] firstClass;
 	delete [] economy;
 }
--- Project1/Plane.h
+++ Project1/Plane.h
@@ -4,25 +4,42 @@
 #include "Seat.h"
 using namespace std;
 
+struct Seat
+{
+    char status;    // '-' if the seat is empty,'X' if the seat is booked
+    bool isBooked;    //True if the seat is empty, false if available
+};
+
 class Plane
 {
 private:
 	Seat **firstClass;	//Dynamic 2D Array of the Seats in first class
 	Seat **economy;		//Dynamic 2D Array of the Seats in economy class
-	int firstClassRow;	//Number of Rows of Seats in first class
-	int firstClassCol;	//Number of Columns of Seats in first class
-	int economyRow;		//Number of Rows of Seats in economy class
-	int economyCol;		//Number of Columns of Seats in economy class
+	int firstClassRows;	//Number of Rows of Seats in first class
+	int firstClassCols;	//Number of Columns of Seats in first class
+	int economyRows;		//Number of Rows of Seats in economy class
+	int economyCols;		//Number of Columns of Seats in economy class
 
 public:
 	Plane();											//Default constructor
-	Plane(int fcRow, int fcCol, int eRow, int eCol);	//Overloaded constructor w/ #s of rows and columns in each class
-	void showAllSeat();									//Print the Seats on the plane
-	void bookASeat();									//Book a Seat on the plane
-	void checkASeat();									//Check if a Seat is booked on the plnae
-	void clearASeat();									//Empty a Seat if it is booked
-	void emptyPlane();									//Empty all of the Seats on the plane
-	~Plane();											//Destructor
+	Plane(int fcRow, int fcCol, int eRow, int eCol);	//Overloaded constructor w/ #s of rows and                          columns in each class
+    
+	void displaySeats();								//Print the Seats on the plane
+	void bookSeat();									//Book a Seat on the plane
+	void checkSeat();									//Check if a Seat is booked on the plnae
+	void clearSeat();									//Empty a Seat if it is booked
+	void clearAllSeats();								//Empty all of the Seats on the plane
+	~Plane(); //Destructor
+    
+    void setFirstClass(int,int);
+    void setEconomyClass(int,int);  //Setters
+    
+    int getFirstClassRows();
+    int getFirstClassCols();    //Getters
+    int getEconomyRows();
+    int getEconomyCols();
+    
+    
 };
 
 #endif
